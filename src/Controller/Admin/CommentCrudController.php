00bfb27e2615e9dd25e0a09d2cf9050389b54c44
plet francois <plet.francois@gmail.com>
2023-11-18 22:15:50 +0100

    add admin db without phpmyadmin

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference Comment')
+            ->setEntityLabelInPlural('Conference Comments')
+            ->setSearchFields(['author', 'text', 'email'])
+            ->setDefaultSort(['createdAt' => 'DESC'])
+        ;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        $fields = parent::configureFields($pageName);
+        $fields[] = AssociationField::new('conference');
+        return $fields;
+        /* return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ]; */
+    }
+    
+}
